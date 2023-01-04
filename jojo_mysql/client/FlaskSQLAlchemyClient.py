d2cfe7dc047e7cd7bff169a25db61a412f89f61f
GengHao <genghao@buaa.edu.cn>
2023-01-04 17:28:36 +0800

    FlaskSQLAlchemy

--- jojo_mysql/client/FlaskSQLAlchemyClient.py
+++ jojo_mysql/client/FlaskSQLAlchemyClient.py
@@ -12,18 +12,31 @@ __all__ = [
 class FlaskSQLAlchemyClient:
     def __init__(self,
                  *, 
-                 app: Flask,
                  host: str,
                  port: int = 3306,
                  user: str, 
                  password: str):
-        self.db = SQLAlchemy()
-        app.config["SQLALCHEMY_DATABASE_URI"] = f"mysql+pymysql://{user}:{password}@{host}:{port}"
-        self.db.init_app(app)            
+        self.host = host 
+        self.port = port 
+        self.user = user 
+        self.password = password
+                 
+        self.db = None 
     
+    def init_app(self,
+                 app: Flask):
+        self.db = SQLAlchemy()
+
+        app.config["SQLALCHEMY_DATABASE_URI"] = f"mysql+pymysql://{self.user}:{self.password}@{self.host}:{self.port}"
+        app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False 
+
+        self.db.init_app(app)   
+        
     def get_table(self,
                   database: str,
-                  table: str) -> PyMySQLTable:
+                  table: str) -> FlaskSQLAlchemyTable:
+        assert self.db is not None 
+                  
         return FlaskSQLAlchemyTable(
             session = self.db.session, 
             database = database, 
